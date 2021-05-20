9ac8f23d3f2d7c5f5adbd4e6efa6ff0cfab6e243
LucianoJunior <lucianojunior4242@gmail.com>
2021-05-20 15:22:25 -0300

    Resoluçao do exercicio 2

---
+++ Exercicios/Fundamentos/bloco_04/dia_02/exercicio_02.js
@@ -0,0 +1,9 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let somatoria = 0;
+
+for (const number of numbers) {
+  somatoria += number;
+}
+
+console.log('Somatoria de todos valores do array: ' + somatoria);
