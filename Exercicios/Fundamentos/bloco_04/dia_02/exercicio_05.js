afb558576ec08f6272f3ebb3d5d5eadb6b00da87
LucianoJunior <lucianojunior4242@gmail.com>
2021-05-20 15:47:49 -0300

    Resoluçao do exercicio 7 e alteraçao no Exercicio 1 e 5

--- Exercicios/Fundamentos/bloco_04/dia_02/exercicio_05.js
+++ Exercicios/Fundamentos/bloco_04/dia_02/exercicio_05.js
@@ -1,11 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let maiorValor = 0;
+let menorValor = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
-  if (numbers[index] > maiorValor) {
-    maiorValor = numbers[index];
+  if (numbers[index] > menorValor) {
+    menorValor = numbers[index];
   }
 }
 
-console.log('O maior valor do array é: ' + maiorValor);
+console.log('O maior valor do array é: ' + menorValor);
