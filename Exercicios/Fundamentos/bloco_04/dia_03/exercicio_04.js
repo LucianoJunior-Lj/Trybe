204b31a3a71737a9b721b8d22419369f7aa29776
LucianoJunior <lucianojunior4242@gmail.com>
2021-05-21 18:09:32 -0300

    Correcao de erro do exercicio 4

--- Exercicios/Fundamentos/bloco_04/dia_03/exercicio_04.js
+++ Exercicios/Fundamentos/bloco_04/dia_03/exercicio_04.js
@@ -3,7 +3,7 @@ let n = 5;
 let asteriscos = '*';
 let espacos;
 
-for (let index = Math.ceil(n / 2); index > 0; index -= 1) {
+for (let index = Math.trunc(n / 2); index >= 0; index -= 1) {
   espacos = ' '.repeat(index);
   
   console.log(espacos + asteriscos);
