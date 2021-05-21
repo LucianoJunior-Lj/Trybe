c7dec69e415bf9090d1a8f93ae49b64cbe4c09c0
LucianoJunior <lucianojunior4242@gmail.com>
2021-05-21 15:17:43 -0300

    Resoluçao do exercicio 1

---
+++ Exercicios/Fundamentos/bloco_04/dia_03/exercicio_01.js
@@ -0,0 +1,13 @@
+let n = 5;
+
+let asteriscos = '*';
+
+for (let index = 0; index < n-1; index++) {
+  asteriscos += '*' ;
+  
+}
+
+for (let index = 0; index < n-1; index++) {
+  console.log(asteriscos);
+  
+}
