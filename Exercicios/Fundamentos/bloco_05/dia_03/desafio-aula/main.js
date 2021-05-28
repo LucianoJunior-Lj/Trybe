624632e9ecc896191217744898d1c84c80d4c383
LucianoJunior <lucianojunior4242@gmail.com>
2021-05-28 20:22:45 -0300

    Resoluçao exercicio 4

--- Exercicios/Fundamentos/bloco_05/dia_03/desafio-aula/main.js
+++ Exercicios/Fundamentos/bloco_05/dia_03/desafio-aula/main.js
@@ -36,21 +36,13 @@ function openPage() {
 }
 myWebpage.addEventListener('dblclick', openPage);
 
-// 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+// 4. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
-
-// Segue abaixo um exemplo do uso de event.target:
-// */
-
-function resetText(event) {
-  // O Event é passado como um parâmetro para a função.
-  event.target.innerText = 'Opção reiniciada';
-  // O event possui várias propriedades, porém a mais usada é o event.target,
-  // que retorna o objeto que disparou o evento.
+function changeColor(element) {
+  element.target.style.color = '#2fc18c';
 }
-
-divUm.addEventListener('dblclick', resetText);
-
-// Não precisa passar o parâmetro dentro do addEventListener. O próprio
-// navegador fará esse trabalho por você, não é legal? Desse jeito, o
-// event.target na nossa função retornará o objeto 'divUm'.
+function changeColorOrigin(element) {
+  element.target.style.color = '';
+}
+myWebpage.addEventListener('mouseover', changeColor);
+myWebpage.addEventListener('mouseout', changeColorOrigin);
