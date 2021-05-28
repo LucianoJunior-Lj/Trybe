2fee2d98f8784db6169d6c36ed2458b4416de141
LucianoJunior <lucianojunior4242@gmail.com>
2021-05-28 20:08:55 -0300

    Resoluçao exercicio 3

--- Exercicios/Fundamentos/bloco_05/dia_03/desafio-aula/main.js
+++ Exercicios/Fundamentos/bloco_05/dia_03/desafio-aula/main.js
@@ -5,10 +5,10 @@ const input = document.getElementById('input');
 const myWebpage = document.getElementById('mySpotrybefy');
 
 /*
-1. Copie esse arquivo e edite apenas ele;
+0. Copie esse arquivo e edite apenas ele;
 
-2. Crie uma função que adicione a classe 'tech' ao elemento selecionado;
-2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?*/
+1. Crie uma função que adicione a classe 'tech' ao elemento selecionado;
+1.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?*/
 function addClass(element) {
   let previousElement = document.querySelector('.tech');
   if (previousElement) {
@@ -20,15 +20,21 @@ function addClass(element) {
 divUm.addEventListener('mouseover', addClass);
 divDois.addEventListener('mouseover', addClass);
 divTres.addEventListener('mouseover', addClass);
-// 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+
+// 2. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
 function changeText(element) {
   document.querySelector('.tech').innerText = element.target.value;
 }
 input.addEventListener('keyup', changeText);
-// 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+
+// 3. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
-// 4.1. Que tal redirecionar para seu portifólio?
+// 3.1. Que tal redirecionar para seu portifólio?
+function openPage() {
+  window.open('https://github.com/LucianoJunior-Lj');
+}
+myWebpage.addEventListener('dblclick', openPage);
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
