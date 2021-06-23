8404e0331889c55cf2918d62efc9259d7fc7348d
LucianoJunior <lucianojunior4242@gmail.com>
2021-06-23 19:02:54 -0300

    Exercicio 4 Parte 2

---
+++ Exercicios/Fundamentos/bloco_07/dia_03/parte_02/exercicio_04.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+// escreva a função findTheNeedle aqui
+function findTheNeedle(words, word) {
+  for (let key = 0; key < words.length; key += 1) {
+    if (words[key] === word) {
+      return key;
+    }      
+  }
+  return -1;
+}
+
+let words = ['house', 'train', 'slide', 'needle', 'book'];
+let expected = 3;
+let output = findTheNeedle(words, 'needle');
+assert.strictEqual(output, expected);
+
+words = ['plant', 'shelf', 'arrow', 'bird'];
+expected = 0;
+output = findTheNeedle(words, 'plant');
+assert.strictEqual(output, expected);
+
+words = ['plant', 'shelf', 'arrow', 'bird'];
+expected = -1;
+output = findTheNeedle(words, 'plat');
+assert.strictEqual(output, expected);
