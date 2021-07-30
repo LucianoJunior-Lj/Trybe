40d3ddf41a12fbb4d05a045b003e2083e8e4cadd
LucianoJunior <lucianojunior4242@gmail.com>
2021-07-30 20:07:30 -0300

    Correção de sinal

--- Exercicios/Fundamentos/bloco_09/dia_02/exercicio_02-05.js
+++ Exercicios/Fundamentos/bloco_09/dia_02/exercicio_02-05.js
@@ -4,7 +4,7 @@ function fetchPromise() {
       Math.pow((Math.floor(Math.random() * 50) + 1), 2)
     ));
     const sum = array.reduce((accumulator, current) => accumulator + current);
-    (sum > 8000) ? resolve(sum) : reject();    
+    (sum < 8000) ? resolve(sum) : reject();    
   })
     .then((sum) => [2, 3, 5, 10].map(number => sum / number))
     .then((array) => console.log(array.reduce((accumulator, current) => accumulator + current)))
