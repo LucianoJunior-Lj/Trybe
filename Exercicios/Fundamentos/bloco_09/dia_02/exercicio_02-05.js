fee43c3937cf14057b3b7cce670e2ce5e5b7e1e8
LucianoJunior <lucianojunior4242@gmail.com>
2021-08-04 20:10:43 -0300

    Exercicio Bonus

--- Exercicios/Fundamentos/bloco_09/dia_02/exercicio_02-05.js
+++ Exercicios/Fundamentos/bloco_09/dia_02/exercicio_02-05.js
@@ -1,14 +1,26 @@
-function fetchPromise() {
-  new Promise((resolve, reject) => {
-    const array = Array.from({ length: 10}, () => (
-      Math.pow((Math.floor(Math.random() * 50) + 1), 2)
-    ));
-    const sum = array.reduce((accumulator, current) => accumulator + current);
-    (sum < 8000) ? resolve(sum) : reject();    
-  })
-    .then((sum) => [2, 3, 5, 10].map(number => sum / number))
-    .then((array) => console.log(array.reduce((accumulator, current) => accumulator + current)))
-    .catch(() => console.log('É mais de oito mil! Essa promise deve estar quebrada!'));
+function sumRandomNumbers() {
+  const array = Array.from({ length: 10 }, () => (
+    Math.pow((Math.floor(Math.random() * 50) + 1), 2)
+  ));
+  const sum = array.reduce((accumulator, current) => accumulator + current, 0);
+
+  if (sum > 8000) {
+    throw new Error();
+  }
+  return sum
+}
+
+async function fetchPromise() {
+  try {
+    const sum = await sumRandomNumbers();
+    const sumArray = [2, 3, 5, 10].map(number => sum / number)
+      .reduce((accumulator, current) => accumulator + current);
+    
+    console.log(sumArray);
+  }
+  catch (error) {
+    console.log('É mais de oito mil! Essa promise deve estar quebrada!')
+  }
 }
 
 fetchPromise();
