edf3b1088f0ee9de25801cfb6b9cd48e14ba0c47
LucianoJunior <lucianojunior4242@gmail.com>
2021-11-19 19:00:09 -0300

    Exercicio 4

--- Exercicios/Back-end/bloco_20/dia_01/exercicios.sql
+++ Exercicios/Back-end/bloco_20/dia_01/exercicios.sql
@@ -10,7 +10,7 @@ GROUP BY JOB_ID
 ORDER BY AVERAGE_SALARY DESC;
 
 -- 4. Escreva uma query que exiba a quantidade de dinheiro necessária para realizar o pagamento de todas as pessoas funcionárias.
-
+SELECT SUM(SALARY) FROM hr.employees;
 
 -- 5. Escreva uma query que exiba quatro informações: o maior salário, o menor salário, a soma de todos os salários e a média dos salários. Todos os valores devem ser formatados para ter apenas duas casas decimais.
 
