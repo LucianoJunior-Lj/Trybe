d08e3c8c40190c5ef3f84e357961c0ad0e4de361
LucianoJunior <lucianojunior4242@gmail.com>
2021-11-19 20:32:41 -0300

    Exercicio 18

--- Exercicios/Back-end/bloco_20/dia_01/exercicios.sql
+++ Exercicios/Back-end/bloco_20/dia_01/exercicios.sql
@@ -79,4 +79,6 @@ SELECT FIRST_NAME, HIRE_DATE FROM hr.employees
 WHERE HIRE_DATE BETWEEN  '1987-07-01' AND '1987-07-31'; 
 
 -- 18: Escreva uma query que exiba as seguintes informações de cada funcionário: nome , sobrenome , tempo que trabalha na empresa (em dias) .
-
+SELECT FIRST_NAME, LAST_NAME, 
+	   DATEDIFF(CURRENT_DATE, HIRE_DATE) AS WORK_DAYS
+FROM hr.employees;
