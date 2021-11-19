2a8ef8fc8fa1203bb7ce368985c2fb91e2542718
LucianoJunior <lucianojunior4242@gmail.com>
2021-11-19 20:09:15 -0300

    Exercicio 14

--- Exercicios/Back-end/bloco_20/dia_01/exercicios.sql
+++ Exercicios/Back-end/bloco_20/dia_01/exercicios.sql
@@ -60,7 +60,7 @@ WHERE LENGTH(FIRST_NAME) >= 8;
 SELECT EMPLOYEE_ID, FIRST_NAME, YEAR(HIRE_DATE) FROM hr.employees; 
 
 -- 14. Escreva uma query que exiba as seguintes informações de cada funcionário: id , primeiro nome e dia do mês no qual foi contratado (exiba somente o dia).
-
+SELECT EMPLOYEE_ID, FIRST_NAME, DAY(HIRE_DATE) FROM hr.employees; 
 
 -- 15. Escreva uma query que exiba as seguintes informações de cada funcionário: id , primeiro nome e mês no qual foi contratado (exiba somente o mês).
 
