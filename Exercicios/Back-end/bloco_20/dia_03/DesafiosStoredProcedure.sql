b4f7c745fc8f57ca07dc8890d2e9d1bdb134014a
LucianoJunior <lucianojunior4242@gmail.com>
2021-12-14 15:27:53 -0300

    Exercicio 2 de stored procedure

--- Exercicios/Back-end/bloco_20/dia_03/DesafiosStoredProcedure.sql
+++ Exercicios/Back-end/bloco_20/dia_03/DesafiosStoredProcedure.sql
@@ -16,7 +16,23 @@ END $$
 DELIMITER ;
 
 -- Exercício 2 - Monte uma procedure que receba como parâmetro de entrada o nome da categoria desejada em uma string e que exiba o id do filme , seu titulo , o id de sua categoria e o nome da categoria selecionada. Use as tabelas film , film_category e category para montar essa procedure.
+USE sakila;
+DELIMITER $$
 
+CREATE PROCEDURE ShowMoviesByCategory(IN searchedCategory VARCHAR(50))
+BEGIN
+  SELECT
+    film.film_id,
+    film.title,
+    category.category_id,
+    category.`name` AS categoryName
+  FROM category
+  INNER JOIN film_category ON category.category_id = film_category.category_id
+  INNER JOIN film ON film_category.film_id = film.film_id
+  WHERE category.`name` = searchedCategory;
+END $$
+
+DELIMITER ;
 
 -- Exercício 3 - Monte uma procedure que receba o email de um cliente como parâmetro de entrada e diga se o cliente está ou não ativo, através de um parâmetro de saída.
 
