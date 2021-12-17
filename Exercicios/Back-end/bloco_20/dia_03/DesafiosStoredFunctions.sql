c5a51d0b62bc0837bb27a6a3caa50cac664c6184
LucianoJunior <lucianojunior4242@gmail.com>
2021-12-17 18:45:24 -0300

    Exercicio 2 de stored functions

--- Exercicios/Back-end/bloco_20/dia_03/DesafiosStoredFunctions.sql
+++ Exercicios/Back-end/bloco_20/dia_03/DesafiosStoredFunctions.sql
@@ -15,7 +15,21 @@ END $$
 DELIMITER ;
 
 -- Exercício 2 - Crie uma function que, dado o parâmetro de entrada inventory_id , retorna o nome do filme vinculado ao registro de inventário com esse id.
+USE sakila;
+DELIMITER $$
 
+CREATE FUNCTION GetMovieName(searchedInventoryId INT)
+RETURNS VARCHAR(30) READS SQL DATA
+BEGIN
+  DECLARE movieName VARCHAR(30);
+  SELECT film.title FROM film
+  INNER JOIN inventory ON film.film_id = inventory.film_id
+  WHERE inventory.inventory_id = searchedInventoryId
+  INTO movieName;
+  RETURN movieName;
+END $$
+
+DELIMITER ;
 
 -- Exercício 3 - Crie uma function que receba uma determinada categoria de filme em formato de texto (ex: 'Action' , 'Horror' ) e retorna a quantidade total de filmes registrados nessa categoria.
 
