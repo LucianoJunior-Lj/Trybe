ca28f7c757e921ca763d5c336ce75cd5ff7d01a7
LucianoJunior <lucianojunior4242@gmail.com>
2021-12-22 12:07:00 -0300

    Exercicio 2 de view

--- Exercicios/Back-end/bloco_21/dia_03/DesafiosView.sql
+++ Exercicios/Back-end/bloco_21/dia_03/DesafiosView.sql
@@ -10,7 +10,16 @@ CREATE VIEW film_with_categories AS
   ORDER BY film.title;
 
 -- Exercício 2 - Crie uma view chamada film_info utilizando as tabelas actor , film_actor e film do banco de dados sakila . Sua view deve exibir o actor_id , o nome completo do ator ou da atriz em uma coluna com o ALIAS actor e o título dos filmes. Os resultados devem ser ordenados pelos nomes de atores e atrizes. Use a imagem a seguir como referência.
-
+CREATE VIEW film_info AS
+  SELECT
+    actor.actor_id,
+    CONCAT(actor.first_name, ' ', actor.last_name) AS `actor`,
+    film.title
+  FROM sakila.actor AS actor
+  INNER JOIN sakila.film_actor AS filmActor ON actor.actor_id = filmActor.actor_id
+  INNER JOIN sakila.film AS film ON film.film_id = filmActor.film_id
+  ORDER BY `actor`;
+  
 -- Exercício 3 - Crie uma view chamada address_info que faça uso das tabelas address e city do banco de dados sakila . Sua view deve exibir o address_id , o address , o district , o city_id e a city . Os resultados devem ser ordenados pelo nome das cidades. Use a imagem abaixo como referência.
 
 -- Exercício 4 - Crie uma view chamada movies_languages , usando as tabelas film e language do banco de dados sakila . Sua view deve exibir o título do filme , o id do idioma e o idioma do filme , como na imagem a seguir.
